be8d3f047b4cb7fbcc2e5a7fc1312f7b58030bc6
kylegranger <kyle.granger@chello.at>
2015-10-17 17:18:41 +0200

    Create simple.h

---
+++ simple.h
@@ -0,0 +1,31 @@
+#ifndef SIMPLE_H
+#define SIMPLE_H
+
+#include "acidtypes.h"
+
+typedef enum ESimpleFormat {
+	ESimpleFormatRgba = 3131,  // 4 channels
+	ESimpleFormatBgra = 3132,  // 4 channels
+	ESimpleFormatRgb  = 3133,  // 3 channels
+	ESimpleFormatBgr  = 3134,  // 4 channels
+	ESimpleFormatL    = 3135,  // 1 channel, luma
+	ESimpleFormatLa   = 3136,  // 2 channels
+} ESimpleFormat;
+
+
+typedef struct TSimpleHeader {
+	// 'SIMPLICE' = 0x4543494c504d4953L
+	int64_t cookie; 
+	// datasize does not include 32-byte header
+	int64_t datasize;
+	int32_t width;
+	int32_t height;
+	// 8, 16, or 32; if 32, then float
+	int32_t bits;
+	// ESimpleFormat
+	int32_t format;
+} TSimpleHeader;
+
+
+#endif
+
